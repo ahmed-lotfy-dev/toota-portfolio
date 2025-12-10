747782cbdba4c0e6c9fb9ed7f3a67b2cc6b4ae10
ahmed-lotfy-dev <elshenawy19@gmail.com>
2025-12-11 00:47:25 +0200

    migrate to use mailtrapsdk

--- config/services.php
+++ config/services.php
@@ -45,4 +45,10 @@ return [
         'email' => env('ADMIN_EMAIL'),
     ],
 
+    'mailtrap-sdk' => [
+        'host' => env('MAILTRAP_HOST', 'send.api.mailtrap.io'),
+        'apiKey' => env('MAILTRAP_API_KEY'),
+        'inboxId' => env('MAILTRAP_INBOX_ID'),
+    ],
+
 ];
